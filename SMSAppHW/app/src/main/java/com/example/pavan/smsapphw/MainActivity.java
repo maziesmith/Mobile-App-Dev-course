d2eaa18954560f37a9be2d65148c9cee74487d09
pavan-kalyan <damalapati.pavan@gmail.com>
2018-10-17 10:37:26 +0530

    added homework app with SMS

--- SMSAppHW/app/src/main/java/com/example/pavan/smsapphw/MainActivity.java
+++ SMSAppHW/app/src/main/java/com/example/pavan/smsapphw/MainActivity.java
@@ -1,19 +1,20 @@
-package com.example.pavan.myappwithsms;
+package com.example.pavan.smsapphw;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import static java.lang.Thread.sleep;
 
 public class MainActivity extends AppCompatActivity {
+
     EditText e1;
     EditText e2;
     String phno;
@@ -31,7 +32,11 @@ public class MainActivity extends AppCompatActivity {
         //MY_PERMISSIONS_REQUEST_SEND_SMS=1;
 
     }
-    public void sendSMSMessage(View v)
+    public void onClick(View v)
+    {
+        sendSMSMessage();
+    }
+    public void sendSMSMessage()
     {
 
         //check if permissions have been granted
@@ -48,8 +53,20 @@ public class MainActivity extends AppCompatActivity {
             }
             return;
         }
-        SmsManager smsmgr =  SmsManager.getDefault();
-        smsmgr.sendTextMessage(phno,null,message,null,null);
+        for(int i=0;i<10;i++) {
+
+
+            SmsManager smsmgr = SmsManager.getDefault();
+            smsmgr.sendTextMessage(phno, null, i+"", null, null);
+            try
+            {
+                sleep(1000);
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
     }
     @Override
     public void onRequestPermissionsResult(int requestCode,String permissions[],int [] grantResults)
@@ -60,7 +77,11 @@ public class MainActivity extends AppCompatActivity {
             {
                 if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED)
                 {
-                    sendSMSMessage(null);
+                    sendSMSMessage();
+                    /*
+                    SmsManager smsmgr =  SmsManager.getDefault();
+                    smsmgr.sendTextMessage(phno,null,message,null,null);
+                    */
                 }
             }
         }
